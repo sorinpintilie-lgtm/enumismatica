5b7b0d31c245d6892054db9cedf3335e1553ea46
sorinpintilie-lgtm <sorinpintilie@example.com>
2025-11-25 12:22:31 +0200

    Remove turbopack config to allow webpack flag

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['shared'],
-  turbopack: {},
 
   webpack: (config, { isServer }) => {
     const path = require('path');
